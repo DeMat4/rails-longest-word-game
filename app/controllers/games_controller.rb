575f0c814ad09ddf5f65b7a68476d2e2ac563dea
matthieu boussicault <boussicaultmatthieu@gmail.com>
2023-05-22 17:40:35 +0200

    rails exo 3

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,47 @@
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    10.times do
+      @letters << ("A".."Z").to_a.sample
+    end
+  end
+
+  def included?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+
+  def compute_score(attempt, time_taken)
+    time_taken > 60.0 ? 0 : (attempt.size * (1.0 - (time_taken / 60.0)))
+  end
+
+  def run_game(attempt, grid, start_time, end_time)
+    # TODO: runs the game and return detailed hash of result (with `:score`, `:message` and `:time` keys)
+    result = { time: end_time - start_time }
+
+    score_and_message = score_and_message(attempt, grid, result[:time])
+    result[:score] = score_and_message.first
+    result[:message] = score_and_message.last
+
+    result
+  end
+
+  def score # def score_and_message(attempt, grid, time)
+    @word = params[:word]
+    if included?(@word.upcase, @letters)
+      if english_word?(@word)
+        score = compute_score(@word, time)
+        [score, "well done"]
+      else
+        [0, "not an english word"]
+      end
+    else
+      [0, "not in the grid"]
+    end
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+end
